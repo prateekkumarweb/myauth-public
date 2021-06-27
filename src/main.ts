e78e9e1dff6a8580808f184c919abd7d2a6dc4ba
Prateek Kumar <prateek@prateekkumar.in>
2021-06-27 19:20:54 +0530

    add redirect for other domain
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,10 @@ import App from "./App.vue";
 import "./firebase";
 import "./index.css";
 
+if (import.meta.env.PROD) {
+  if (window.location.host !== "myauth.prateekkumar.in") {
+    window.location.replace("https://myauth.prateekkumar.in");
+  }
+}
+
 createApp(App).use(createPinia()).mount("#app");
